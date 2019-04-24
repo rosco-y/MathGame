a8258d32cba8bceea23f98ffd66dc7e4444e5548
Ross Ylitalo <ross.ylitalo@gmail.com>
2019-04-24 17:07:11 -0400

    Item now slides over counter.
    Item has a "smooth" material (instead of the counter or belt, or whatever it's on.)  This way it doesn't matter what the item is on, it supplies it's own (lack of) friction.

---
+++ MathGame/Assets/_SCRIPTS/cMoveItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cMoveItem : MonoBehaviour
+{
+
+    public float _speed = 3f;
+
+    Rigidbody _rig;
+    private void Awake()
+    {
+        _rig = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        _rig.velocity = new Vector3(_speed, 0f);
+    }
+}
