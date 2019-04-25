b64c17a968af3372e9dbcf4073de820170f656ed
Ross Ylitalo <ross.ylitalo@gmail.com>
2019-04-24 22:44:47 -0400

    Have Triggers which Get and Restore Items to and from an object pool.

---
+++ MathGame/Assets/_SCRIPTS/cRemoveItemTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cRemoveItemTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        cItemPricer item = other.GetComponent<cItemPricer>();
+        item.gameObject.SetActive(false);
+        cItemPool.Instance.RestoreToPool(item);
+    }
+}
